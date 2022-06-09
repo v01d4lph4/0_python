cbcd63bb7eb66ffbe6a254057cd73c31649f5c23
v01d4lph4 <v01d4lph4@gmail.com>
2022-06-09 14:06:11 +0530

    Update 12_reduce.py

--- 12_reduce.py
+++ 12_reduce.py
@@ -2,13 +2,13 @@
 
 # reduce() performs functional computation called folding by taking a function and an iterable 
 # (eg: list, tuple, dict etc.) as arguments and result is returned after applying the function 
-# on the iterable cumulateively.
+# on the iterable cumulatively.
 
 # ⚙️ syntax   : functools.reduce(func, iterable[, initializer])
 # func        : two-argument function that will be applied to iterable to compute a final value.
 # iterable    : can be built-in/user-defined iterable (like: list, string, dict)
 # initializer : (optional) placed before the iterable during computation, default when the iterable is empty.
-# returns a single value as output which is the result of whole iterable getting reduced cumulateively.
+# returns a single value as output which is the result of whole iterable getting reduced cumulatively.
 
 # an example to reduce a list of numbers to its sum
 from functools import reduce 
