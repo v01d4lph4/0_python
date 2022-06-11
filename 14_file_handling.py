9f414ff3a9e88cf782f45e74ac7903470e397cc0
v01d4lph4 <v01d4lph4@gmail.com>
2022-06-12 00:08:42 +0530

    Update 14_file_handling.py

--- 14_file_handling.py
+++ 14_file_handling.py
@@ -5,7 +5,13 @@
 
 # ⚙️ syntax   : open(filename, mode)
 # filename    : path to file (text/binary)
-# mode        : represents the purpose of the opening file (read/write/append/create)
+# mode        : represents the purpose of the opening file, supported modes are:
+#              - r: open an existing file for a read operation.
+#              - w: open an existing file for a write operation. If the file already contains some data then it will be overridden.
+#              - a:  open an existing file for append operation. It won’t override existing data.
+#              - r+:  To read and write data into the file. The previous data in the file will be overridden.
+#              - w+: To write and read data. It will override existing data.
+#              - a+: To append and read data from the file. It won’t override existing data.
 # returns a iterable file object
 
 # to read a file 
