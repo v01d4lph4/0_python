480d066ee604ecc8e1bb17fadd8327689aca9f43
v01d4lph4 <v01d4lph4@gmail.com>
2022-06-15 23:45:23 +0530

    Update 17_all_unique.py

--- 17_all_unique.py
+++ 17_all_unique.py
@@ -4,7 +4,7 @@
 
 # you can cast the list into a set() to eliminate duplicate 
 # elements and compare the lengths. 
-ex_list_0 = [1,2,2,3,4,5,5,6]
+ex_list_0 = [1, 2, 2, 3, 4, 5, 5, 6]
 print(len(ex_list_0) == len(set(ex_list_0)))
 #> False
 
@@ -13,7 +13,7 @@ print(len(ex_list_1) == len(set(ex_list_1)))
 #> True
 
 # 🎁 bonus: to check if a string has unique charcters, 
-# a similar approach can be used by cast str to list.
+# a similar approach can be used by cast str to list
 name = "el0n_mu$k"
 name_list = list(name)
 #> ['e', 'l', '0', 'n', '_', 'm', 'u', '$', 'k']
