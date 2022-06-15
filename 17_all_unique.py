b04bff7d53aac2e052eecb9770814112689bdbc0
v01d4lph4 <v01d4lph4@gmail.com>
2022-06-15 23:44:37 +0530

    Update 17_all_unique.py

--- 17_all_unique.py
+++ 17_all_unique.py
@@ -13,7 +13,7 @@ print(len(ex_list_1) == len(set(ex_list_1)))
 #> True
 
 # 🎁 bonus: to check if a string has unique charcters, 
-# a similar approach can be used by cast str to list
+# a similar approach can be used by cast str to list.
 name = "el0n_mu$k"
 name_list = list(name)
 #> ['e', 'l', '0', 'n', '_', 'm', 'u', '$', 'k']
